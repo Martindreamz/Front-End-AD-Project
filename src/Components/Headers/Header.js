b59575158f5f087f306e1e9c6e1c2a96e1184325
WayneMyo <waiyankm93@gmail.com>
2020-08-17 01:27:55 +0800

    Header - Nav bar (mobile mode) added and css changes

--- src/Components/Headers/Header.js
+++ src/Components/Headers/Header.js
@@ -1,42 +1,60 @@
 import React, { Component } from "react";
 import ReactDom from "react-dom";
-import './Header.css';
-import { domain } from '../../Configurations/Config';
-import HomeIcon from '@material-ui/icons/Home';
-import AccountBoxIcon from '@material-ui/icons/AccountBox';
-import ExitToAppIcon from '@material-ui/icons/ExitToApp';
-
+import "./Header.css";
+import { domain } from "../../Configurations/Config";
+import HomeIcon from "@material-ui/icons/Home";
+import AccountBoxIcon from "@material-ui/icons/AccountBox";
+import ExitToAppIcon from "@material-ui/icons/ExitToApp";
 
 class Header extends React.Component {
-    constructor() {
-        super()
-        this.state = {
-            name: "Daryl",
-            logState: "Login"
-        }
-    }
+  constructor() {
+    super();
+    this.state = {
+      name: "Daryl",
+      logState: "Login",
+    };
+  }
 
-    homeButtonAction = () => {
-        window.location.href = domain
-    }
+  homeButtonAction = () => {
+    window.location.href = domain;
+  };
 
-    render() {
-        return (
-            <nav className="header">
-                <HomeIcon onClick={this.homeButtonAction} />
-                <div className="userHeader">
-                    <div className="userHeaderName">
-                        <AccountBoxIcon />
-                        <p>{this.state.name}</p>
-                    </div>
-                    <div className="userHeaderState">
-                        <ExitToAppIcon />
-                        <p>{this.state.logState}</p>
-                    </div>
-                </div>
-            </nav>
-        )
-    }
-} 
+  toggleButtonAction = () => {
+    document
+      .getElementsByClassName("navbar-links")[0]
+      .classList.toggle("active");
+  };
+
+  render() {
+    return (
+      <nav className="navbar">
+        <div class="brand-title">
+          <HomeIcon onClick={this.homeButtonAction} />
+        </div>
+        <a href="#" class="toggle-button" onClick={this.toggleButtonAction}>
+          <span class="bar"></span>
+          <span class="bar"></span>
+          <span class="bar"></span>
+        </a>
+        <div class="navbar-links">
+          <ul>
+            <li>
+              <a href="#">
+                <AccountBoxIcon />
+                {this.state.name}
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <ExitToAppIcon />
+                {this.state.logState}
+              </a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    );
+  }
+}
 
 export default Header;
