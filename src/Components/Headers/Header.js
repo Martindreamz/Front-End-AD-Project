6d038559141f0ed7503decc7831bff52c00ecbd1
WayneMyo <waiyankm93@gmail.com>
2020-08-17 17:07:02 +0800

    Header Icons enlarged

--- src/Components/Headers/Header.js
+++ src/Components/Headers/Header.js
@@ -6,57 +6,60 @@ import HomeIcon from "@material-ui/icons/Home";
 import AccountBoxIcon from "@material-ui/icons/AccountBox";
 import ExitToAppIcon from "@material-ui/icons/ExitToApp";
 
-
 class Header extends React.Component {
-    constructor() {
-        super();
-        this.state = {
-            name: "Daryl",
-            logState: "Login",
-        };
-    }
+  constructor() {
+    super();
+    this.state = {
+      name: "Daryl",
+      logState: "Login",
+    };
+  }
 
-    homeButtonAction = () => {
-        window.location.href = domain;
+  homeButtonAction = () => {
+    window.location.href = domain;
+  };
+
+  toggleButtonAction = () => {
+    document
+      .getElementsByClassName("navbar-links")[0]
+      .classList.toggle("active");
+  };
+
+  render() {
+    const iconStyle = {
+      width: 35,
+      height: 35,
+      marginTop: 5,
+      marginLeft: 15,
+      marginRight: 15,
     };
 
-    toggleButtonAction = () => {
-        document
-          .getElementsByClassName("navbar-links")[0]
-          .classList.toggle("active");
-      };
+    return (
+      <nav className="navbar">
+        <div className="brand-title">
+          <HomeIcon onClick={this.homeButtonAction} style={iconStyle} />
+          <p>Stationery Management System</p>
+        </div>
+        <a href="#" className="toggle-button" onClick={this.toggleButtonAction}>
+          <span class="bar"></span>
+          <span class="bar"></span>
+          <span class="bar"></span>
+        </a>
+        <div className="navbar-links">
+          <ul>
+            <li>
+              <AccountBoxIcon style={iconStyle} />
+              <a href="#">{this.state.name}</a>
+            </li>
+            <li>
+              <ExitToAppIcon style={iconStyle} />
+              <a href="#">{this.state.logState}</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    );
+  }
+}
 
-      render() {
-        return (
-          <nav className="navbar">
-            <div class="brand-title">
-              <HomeIcon onClick={this.homeButtonAction} />
-              Stationery Management System
-            </div>
-            <a href="#" class="toggle-button" onClick={this.toggleButtonAction}>
-              <span class="bar"></span>
-              <span class="bar"></span>
-              <span class="bar"></span>
-            </a>
-            <div class="navbar-links">
-              <ul>
-                <li>
-                  <a href="#">
-                    <AccountBoxIcon />
-                    {this.state.name}
-                  </a>
-                </li>
-                <li>
-                  <a href="#">
-                    <ExitToAppIcon />
-                    {this.state.logState}
-                  </a>
-                </li>
-              </ul>
-            </div>
-          </nav>
-        );
-      }
-    }
-    
 export default Header;
